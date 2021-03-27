db400739eb95a48ebdee23bfd28fe1a29757048d
femilabi <oderinwalefemi150@gmail.com>
2021-03-27 04:14:23 +0100

    Removed a commented line from config.prod.js

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,4 +1,3 @@
-// https://www.freecodecamp.org/news/securing-node-js-restful-apis-with-json-web-tokens-9f811a92bb52/
 module.exports = {
     VIEW_DIR: "http://localhost:3000/", // Development
     RESPONSE_TYPE: "JSON", // Production
